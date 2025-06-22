94f14ea5d4bb1131e6630e33429d82dbf9e86087
Chinmayee <chinmayee.arun.yadav@gmail.com>
2025-06-22 13:41:04 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,12 @@
+# Find the Missing Number in an Array
+# A list contains n distinct numbers taken from 0, 1, 2, ..., n, but one number is missing.
+# Write a function to find the missing number.
 
+def find_missing_number(arr: list) -> int:
+  n = len(arr)
+  expected_sum = (n*(n+1))/2
+  actual_sum =sum(arr)
+  return expected_sum - actual_sum
+
+arr1 = [3,0,1]
+missing_num = find_missing_number(arr1)
